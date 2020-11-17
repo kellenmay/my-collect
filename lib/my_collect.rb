9c599cd5fcdac465fb5606b34de80316ca80377f
Kellen <kellenbmay@gmail.com>
2020-11-17 13:44:50 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    collection
+  end 
 
+empty_array = []
 
+  my_collect(empty_array) do |x|
+    counter += 1
+  end
